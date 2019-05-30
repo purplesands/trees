f849cfcac86b5617d6982d9e4727a86b011e9af9
purplesands <mullenjoe13@gmail.com>
2019-05-30 15:28:01 -0400

    npm

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import anime from 'lib/anime.es.js';
+import anime from 'animejs'
 
 window.addEventListener('DOMContentLoaded', e => {
 
@@ -9,6 +9,13 @@ window.addEventListener('DOMContentLoaded', e => {
       abt.innerHTML = ``
     })
 
+    anime({
+      targets: 'div',
+      translateX: 250,
+      rotate: '1turn',
+      backgroundColor: '#FFF',
+      duration: 800
+    });
 
 
     console.log('DOM fully loaded and parsed');
