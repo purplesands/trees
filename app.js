51fdc0ca96e17989d1ee02ccf530e4aa605955dc
purplesands <mullenjoe13@gmail.com>
2019-06-17 19:40:54 -0400

    index2

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 $(document).ready(function(){
 
             var scroll_pos = 0;
             $(document).scroll(function() {
                 scroll_pos = $(this).scrollTop();
                 if(scroll_pos > 90) {
-                    $(".head").css({'background-color':'transparent', 'opacity':'0.8', 'transform': 'scale(1, 0.3)','top':'-3em'});
+                    $(".head").css({'background-color':'transparent', 'opacity':'0.7', 'transform': 'scale(1, 0.5)','top':'-2em'});
                 } else {
                     $(".head").css({'background-color':'transparent', 'opacity':'1', 'transform': 'scale(1, 1)','top':'0.5em'});
                 }
